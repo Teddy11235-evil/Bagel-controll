98d5d3c86cc050b146fd0199c99c4eab7545b10a
Teddy11235-evil <hackerzero2012@gmail.com>
2025-11-12 10:09:05 +0100

    Implement basic worker function with CORS support

---
+++ netlify/functions/worker.js
@@ -0,0 +1,18 @@
+// Additional worker management functions
+exports.handler = async (event, context) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ message: 'Workers API ready' })
+  };
+};
